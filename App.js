b87fdd7a5a8241b73659a723e2d8996cad34153e
yaroslavnikiforov <yaroslav.nikiforov.93@gmail.com>
2020-08-12 12:26:44 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import {View} from 'react-native';
+
+import {GLView} from 'expo-gl';
+
+const App: () => React$Node = () => (
+  <GLView style={{width: 300, height: 500}} />
+);
+
+export default App;
